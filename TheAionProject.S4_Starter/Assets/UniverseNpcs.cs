79940edd6f81bc88dd16a191a74fd0275d6fa7eb
Madeleine Woodbury <woodbu7@ad.nmc.edu>
2018-04-09 16:46:43 -0400

    Add project files.

---
+++ TheAionProject.S4_Starter/Assets/UniverseNpcs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject
+{
+    public static partial class UniverseObjects
+    {
+        public static List<Npc> Npcs = new List<Npc>()
+        {
+            new Civilian
+            {
+                Id = 1,
+                Name = "Man with Stripped Hat",
+                SpaceTimeLocationID = 2,
+                Description = "A tall man in baggy pants with a red, pin stripped hat",
+                Messages = new List<string>()
+                {
+                    "Grettings stranger. You are not from these parts as I can see",
+                    "You will find what you are looking ofr with the Pink Gorilla",
+                    "I once attented the Academy. The felt I needed more candles"
+                }
+            },
+
+            new Civilian
+            {
+                Id = 2,
+                Name = "Timothy Sergent",
+                SpaceTimeLocationID = 1,
+                Description = "The lead developer of the Stratus Flux Capacitor",
+                Messages = new List<string>()
+                {
+                    "I have to go. Godd buy!",
+                    "It was always meant for good. We had no idea",
+                    "Is that man following you?"
+                }
+            },
+
+            new Civilian
+            {
+                Id = 3,
+                Name = "Thorian Diplomat",
+                SpaceTimeLocationID = 2,
+                Description = "A thorian diplomat dresses in traditional phlox and cords"
+            }
+        };
+        
+        
+    }
+
+}
---
+++ TheAionProject.S4_Starter/Models/Civilian.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject
+{
+    public class Civilian : Npc, ISpeak
+    {
+        public override int Id { get; set; }
+        public override string Description { get; set; }
+        public List<string> Messages { get; set; }
+
+        /// <summary>
+        /// generate a message or use default
+        /// </summary>
+        /// <returns></returns>
+        public string Speak()
+        {
+            if (this.Messages != null)
+            {
+                return GetMessage();
+            }
+            else
+            {
+                return $"My name is {base.Name} and I am a {base.Race}";
+            }
+        }
+
+        /// <summary>
+        /// randomly select a message from the list of messages
+        /// </summary>
+        /// <returns></returns>
+        private string GetMessage()
+        {
+            Random r = new Random();
+            int messageIndex = r.Next(0, Messages.Count());
+            return Messages[messageIndex];
+        }
+    }
+}
